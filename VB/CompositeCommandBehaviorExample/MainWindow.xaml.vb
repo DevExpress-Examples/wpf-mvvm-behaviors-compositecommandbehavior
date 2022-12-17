948be80324544cd23258930d779b22a0a9b32779
DevExpressExampleBot <fakeEmail>
2022-12-17 19:37:08 +0400

    Source auto update [skip ci]

--- VB/CompositeCommandBehaviorExample/MainWindow.xaml.vb
+++ VB/CompositeCommandBehaviorExample/MainWindow.xaml.vb
@@ -1,11 +1,10 @@
-﻿Imports System.Windows
-
 Namespace CompositeCommandBehaviorExample
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits DevExpress.Xpf.Core.DXWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
     End Class
 End Namespace
--- VB/CompositeCommandBehaviorExample/ViewModel/MainViewModel.vb
+++ VB/CompositeCommandBehaviorExample/ViewModel/MainViewModel.vb
@@ -1,45 +1,43 @@
-﻿Imports System
-Imports System.Linq
 Imports DevExpress.Mvvm
-Imports DevExpress.Mvvm.POCO
-Imports System.Collections.Generic
+Imports DevExpress.Mvvm.DataAnnotations
 
 Namespace CompositeCommandBehaviorExample.ViewModel
+
     Public Class MainViewModel
         Inherits ViewModelBase
 
-        Protected Sub New()
-        End Sub
-        Public Shared Function Create() As MainViewModel
-            Return ViewModelSource.Create(Function() New MainViewModel())
-        End Function
+        Public Overridable Property Text As String
 
-        Public Overridable Property Text() As String
-        Public Overridable Property SavedText() As String
-        Private ReadOnly Property IsSaved() As Boolean
+        Public Overridable Property SavedText As String
+
+        Private ReadOnly Property IsSaved As Boolean
             Get
-                Return SavedText = Text
+                Return Equals(SavedText, Text)
             End Get
         End Property
-        Protected ReadOnly Property MessageService() As IMessageBoxService
+
+        Protected ReadOnly Property MessageService As IMessageBoxService
             Get
                 Return GetService(Of IMessageBoxService)()
             End Get
         End Property
 
+        <Command>
         Public Sub Save()
             SavedText = Text
         End Sub
+
         Public Function CanSave() As Boolean
             Return Not IsSaved
         End Function
+
+        <Command>
         Public Sub Close()
-            If (Not IsSaved) AndAlso MessageService.ShowMessage("Are you sure to close unsaved document", "Warning", MessageButton.YesNo) = MessageResult.No Then
-                Return
-            End If
+            If Not IsSaved AndAlso MessageService.ShowMessage("Do you want to close the document and lost unsaved changes?", "Warning", MessageButton.YesNo) = MessageResult.No Then Return
             Text = String.Empty
             SavedText = String.Empty
         End Sub
+
         Public Function CanClose() As Boolean
             Return Not String.IsNullOrEmpty(Text)
         End Function
